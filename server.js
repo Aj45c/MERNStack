047399b92c9c5254cb178cae51a75d85a259780c
Anthony <anthony.jose53@gmail.com>
2022-06-13 10:42:14 -0400

    Added schema

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
 const app = express();
 
@@ -12,6 +13,22 @@ mongoose.connect(MONGODB_URL);
 mongoose.connection
 .on('connected', () => console.log ('Mongodv is CO-NEECTED'))
 .on('error', (err) => console.log ('Error ma boy' + err.message))
+//Setup Model--------------------------------
+const peopleSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    title: String,
+});
+
+const People = mongoose.model('People', peopleSchema)
+
+//Middleware--------------------------------
+app.use(cors()); //Access-Control-Allow: "*"
+app.use(morgan('dev'));
+app.use(express.json());
+//this creates req.body from incoming JSON request bodies
+//app.use(express.urlencoded({extended: false}))
+// ^^^ only when express serves html soo like the ejs files
 
 //-------------------------------------------
 app.get('/' , (req, res) => {
